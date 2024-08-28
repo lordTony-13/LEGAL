5d7ee0b211f508df428729e5aae6740f81d40d20
Safal Antony <133263566+lordTony-13@users.noreply.github.com>
2024-08-28 16:23:16 +0530

    Initial commit

---
+++ legal/src/global.d.ts
@@ -0,0 +1,8 @@
+declare module '@heroicons/react/solid' {
+    import { FC, SVGProps } from 'react';
+  
+    export const SunIcon: FC<SVGProps<SVGSVGElement>>;
+    export const MoonIcon: FC<SVGProps<SVGSVGElement>>;
+    // Add other icons you use here
+  }
+  
